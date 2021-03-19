6ee223a6614e05acf66e9acfce18ab4e86d0d89d
Vlad Kokhan <vvlad0202@gmail.com>
2021-03-19 16:35:53 +0300

    feature: add todos

--- src/AddTodo/index.tsx
+++ src/AddTodo/index.tsx
@@ -1,15 +1,28 @@
-import React, {FC} from 'react';
-import {Text, View, Button, TextInput} from 'react-native';
+import React, {FC, useState} from 'react';
+import {View, Button, TextInput, Alert} from 'react-native';
 import {AddTodoProps} from './interfaces';
 import {styles} from './styles';
 
 export const AddTodo: FC<AddTodoProps> = ({onAddTodo}) => {
+  const [text, setText] = useState('');
   const handlerPress = () => {
-    onAddTodo('Todo');
+    if (text.trim()) {
+      onAddTodo(text);
+      setText('');
+    } else {
+      Alert.alert('Невозможно добавить пустое дело');
+    }
   };
   return (
     <View style={styles.block}>
-      <TextInput style={styles.input} />
+      <TextInput
+        style={styles.input}
+        onChangeText={setText}
+        value={text}
+        placeholder="Введите название дела"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
       <Button title="Добавить" onPress={handlerPress} />
     </View>
   );
